472301b4cbe03c90b685b8f720cc04f05c2c57d8
Martinitu <martinitu@gmail.com>
2022-09-01 11:28:09 -0500

    exercise 04 finish

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...manyMoreArgs) {
+    for (let i = 0; i < manyMoreArgs.length; i++){
+  let index = arr.indexOf(manyMoreArgs[i])
+  if (index > -1) {
+    arr.splice(index, 1);
+  
+}
+    }
+return arr;
 };
 
 // Do not edit below this line
