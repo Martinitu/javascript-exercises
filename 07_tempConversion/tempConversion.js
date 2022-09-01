2ed1fc531700aef0649d2b8b45dbd420a0f76b44
Martinitu <martinitu@gmail.com>
2022-09-01 11:25:36 -0500

    exercise 7 finish

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempinC = (temp - 32) * (5/9);
+  let result = Math.round(tempinC * 10) / 10;
+  return result
 };
-
-const ctof = function() {
-
+//[°C] = ([°F] − 32) × 5⁄9
+const ctof = function(temp) {
+  let tempinF = temp * (9/5) + 32;
+  let result = Math.round(tempinF * 10) / 10;
+  return result
 };
-
+//[°F] = [°C] × 9⁄5 + 32 
 // Do not edit below this line
 module.exports = {
   ftoc,
