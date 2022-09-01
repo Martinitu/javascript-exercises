1e78c82f05682e28bd06f44c582f9dd2feb86e22
Martinitu <martinitu@gmail.com>
2022-09-01 11:25:36 -0500

    exercise 5 finish

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) { 
+    let firstNum
+    let secondNum
+    if (typeof(numOne) !== "number" || typeof(numTwo) !== "number" ) {
+        return "ERROR"
+    }
+    else if (numOne > numTwo) {
+        firstNum = numTwo;
+        secondNum = numOne;
+    } else  { 
+        firstNum = numOne;
+        secondNum = numTwo;
+    }
+    let result = 0;
+    for (let i = firstNum; i <= secondNum; i++) {
+        result +=  i ;
+    }
+    if (result < 0) {
+        return "ERROR"
+    } else {
+    return result;
+    }
 };
 
 // Do not edit below this line
